bfc8a9cfea103ee0e3919b6d14df03c671b9252f
eieishwezin25 <hninhinaye@aceplussolutons.com>
2017-08-24 00:44:59 +0630

    merge h3 and h2 branch

--- create.php
+++ create.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-<form method="post" action="store.php">
-	<input type="hidden" name="emp_id">
-	Name:<input type="text" name="emp_name"><br>
-	Address:<input type="text" name="emp_address"><br>
-	Salary:<input type="text" name="emp_salary"><br>
-	<input type="submit" value="Add" name="">
-</form>
+<?php
+echo "one testing";
+echo "two testing";
 
-</body>
-</html>
+?>
