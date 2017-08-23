2fbadf9980c4028aa985a1bf3314c2c2ff54e93b
eieishwezin25 <hninhinaye@aceplussolutons.com>
2017-08-24 00:05:12 +0630

    stage all in master

---
+++ create1.php
@@ -0,0 +1,26 @@
+<form method="post" action="store1.php">
+<table>
+	<tr>
+		<td>Name<input type="text" name="name"></td>
+	</tr>
+	<tr>
+		<td>Age<input type="text" name="age"></td>
+	</tr>
+	<tr>
+		<td>Address<input type="text" name="address"></td>
+	</tr>
+	<tr>
+		<td>E-Mail<input type="text" name="email"></td>
+	</tr>
+	<tr>
+		<td>Phone<input type="text" name="phone"></td>
+	</tr>
+	<tr>
+	
+		<td>
+			<input type="submit" value="ADD" name="">
+		</td>
+	</tr>
+</table> 
+	
+</form>
